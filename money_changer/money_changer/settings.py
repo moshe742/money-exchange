9c76aac073108494d4a44f8bb4dbe2910202a380
Moshe Nahmias <moshegrey@gmail.com>
2021-04-10 21:24:03 +0300

    fix allowed hosts

--- money_changer/money_changer/settings.py
+++ money_changer/money_changer/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('MONEY_EXCHANGE_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['exchange.moshe742.name', 'localhost']
 
 
 # Application definition
